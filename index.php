16f4ad9d9f8980789c5a4443c524afd215466765
e-turner1 <ericbennetturner@hotmail.com>
2024-09-28 21:42:57 -0400

    Update index.php

--- index.php
+++ index.php
@@ -42,6 +42,26 @@ for ($i = 0; $i < count($member_data); $i++){
         </div><!--//row-->
     </header>';
 }
+function display_member_card($member, $index) {
+    $age = calculate_age($member['dob']);
+    echo '<header class="resume-header pt-4 pt-md-0">
+        <div class="row">
+            <div class="col-block col-md-auto resume-picture-holder text-center text-md-start">
+                <img class="picture" src="assets/images/' . $member['member'] . '.jpg" style="height: 200px;" alt="">
+            </div><!--//col-->
+            <div class="col">
+                <div class="row p-4 justify-content-center justify-content-md-between">
+                    <div class="primary-info col-auto">
+                        <h1 class="name mt-0 mb-1 text-white text-uppercase">' . $member['member'] . '</h1>
+                        <div class="title mb-3">' . $member['title'] . '</div>
+                        <div class="age mb-3">Age: ' . $age . '</div>
+                        <a href="detail.php?index=' . $index . '" class="btn btn-secondary">See full profile</a>
+                    </div><!--//primary-info-->
+                </div><!--//row-->
+            </div><!--//col-->
+        </div><!--//row-->
+    </header>';
+}
 ?>
 <!DOCTYPE html>
 <html lang="en"> 
