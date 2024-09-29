600317cf1a2c4e7c1420e143d1e8a5bea5d4ea1a
e-turner1 <ericbennetturner@hotmail.com>
2024-09-29 16:28:38 -0400

    Update detail.php

--- detail.php
+++ detail.php
@@ -534,6 +534,32 @@ $resume_data = [
 		], 
 	],
 ];
+function calculate_age($dob) {
+    $dob = new DateTime($dob);  
+    $now = new DateTime();      
+    $age = $now->diff($dob);    
+    return $age->y; 
+}
+for ($i = 0; $i < count($member_data); $i++){
+    $member = $member_data[$i];
+    $age = calculate_age($member['dob']);  
+    echo '<header class="resume-header pt-4 pt-md-0">
+        <div class="row">
+            <div class="col-block col-md-auto resume-picture-holder text-center text-md-start">
+                <img class="picture" src="assets/images/' . $member['member'] . '.jpg" style="height: 200px;" alt="">
+            </div><!--//col-->
+            <div class="col">
+                <div class="row p-4 justify-content-center justify-content-md-between">
+                    <div class="primary-info col-auto">
+                        <h1 class="name mt-0 mb-1 text-white text-uppercase">' . $member['member'] . '</h1>
+                        <div class="title mb-3">' . $member['title'] . '</div>
+                        <div class="age mb-3">Age: ' . $age . '</div>  <!-- Display age here -->
+                        <a href="detail.php?index=' . $i . '" class="btn btn-secondary">See full profile</a>
+                    </div><!--//primary-info-->
+                </div><!--//row-->
+            </div><!--//col-->
+        </div><!--//row-->
+    </header>';
 $index=$_GET['index'];
 $resume_intro = $resume_data[$index];
 $resume_contacts = $resume_intro['contacts'];
